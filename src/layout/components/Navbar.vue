31991df057bea74c0dc72b896534e28f0a5c85ee
1660031035 <102581655+1660031035@users.noreply.github.com>
2022-06-06 16:40:24 +0800

    显示登录用户名和头像

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -10,8 +10,8 @@
     <div class="right-menu">
   <el-dropdown class="avatar-container" trigger="click">
     <div class="avatar-wrapper">
-      <img src="@/assets/common/bigUserHeader.png" class="user-avatar">
-      <span class="name">{{ $store.state.user.userInfo.username }}</span>
+      <img :src="userInfo.staffPhoto" class="user-avatar">
+      <span class="name">{{ userInfo.username }}</span>
       <i class="el-icon-caret-bottom" style="color:#fff" />
     </div>
     <el-dropdown-menu slot="dropdown" class="user-dropdown">
@@ -35,7 +35,7 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapState } from 'vuex'
 import Breadcrumb from '@/components/Breadcrumb'
 import Hamburger from '@/components/Hamburger'
 
@@ -48,7 +48,8 @@ export default {
     ...mapGetters([
       'sidebar',
       'avatar'
-    ])
+    ]),
+    ...mapState('user', ['userInfo']),
   },
   methods: {
     toggleSideBar() {
