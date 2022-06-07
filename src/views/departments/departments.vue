62e72a8254e773d44428bd704fe62e108db02929
lh-fish <1660031035@qq.com>
2022-06-07 21:42:47 +0800

    组织架构-实现树形组件-接口真实数据渲染和处理树形结构

--- src/views/departments/departments.vue
+++ src/views/departments/departments.vue
@@ -26,51 +26,68 @@
             </el-col>
           </el-row>
           <!-- 放置一个el-tree组件 -->
-        <el-tree :data="list" default-expand-all/>
-        <el-row type="flex" justify="space-between" align="middle" style="height: 40px width: 100%;">
-            <el-col :span="20">
-              <svg-icon icon-class="home" /><span>江苏传智播客教育科技股份有限公司</span>
-            </el-col>
-            <el-col :span="4">
-              <el-row type="flex" justify="end">
-                <!-- 两个内容 -->
-                <el-col>负责人</el-col>
-                <!-- 下拉菜单 element -->
-                <el-col>
-                  <el-dropdown>
-                    <span>
-                      操作<i class="el-icon-arrow-down" />
-                    </span>
-                    <el-dropdown-menu slot="dropdown">
-                      <el-dropdown-item>添加子部门</el-dropdown-item>
-                    </el-dropdown-menu>
-                  </el-dropdown>
-                </el-col>
-              </el-row>
-            </el-col>
-          </el-row>
+        <!-- default-expand-all 默认展开 -->
+        <el-tree default-expand-all :data="data" :props="defaultProps">
+        <template class="custom-tree-node" slot-scope="{ data }">
+        <el-row
+          type="flex"
+          justify="space-between"
+          align="middle"
+          style="height: 40px; width: 100%;"
+        >
+          <el-col :span="20">
+            <svg-icon icon-class="home" /><span>{{ data.name }}</span>
+          </el-col>
+          <el-col :span="4">
+            <el-row type="flex" justify="end">
+              <!-- 两个内容 -->
+              <el-col>{{ data.manager }}</el-col>
+              <!-- 下拉菜单 element -->
+              <el-col>
+                <el-dropdown>
+                  <span> 操作<i class="el-icon-arrow-down" /> </span>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                    <el-dropdown-item>编辑部门</el-dropdown-item>
+                    <el-dropdown-item>删除部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+      </template>
+        </el-tree>
       </el-card>  
       
     </div>
   </div>
 </template>
 <script>
+import { getDepartments } from '@/api/departments'
+import { tranListToTreeData } from '@/utils'
 export default {
   data() {
-    return {
-      // 依赖一份树形数据
-      list: [{
-        label: '财务部',
-        children: [
-          {
-            label: '财务核算部'
-          },
-          {
-            label: '税务核算部'
-          }
-        ]
-      }]
+      return {
+        data: [],
+        defaultProps: {
+          children: 'children',
+          label: 'lable'
+        }
+      };
+    },
+  created() {
+    this.loadDepartments()
+  },
+  methods: {
+    async loadDepartments() {
+      const res = await getDepartments()
+      res.data.depts.shift()
+      console.log(res)
+      // 调用提前封装好的根据函数来做格式转换
+      this.data = tranListToTreeData(res.data.depts)
     }
   }
 }
+
 </script>
