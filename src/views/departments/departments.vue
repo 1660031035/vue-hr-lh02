d9a5f391a06fea2a64edf660f8eb19d330a6dcd7
lh-fish <1660031035@qq.com>
2022-06-07 21:20:29 +0800

    移动el-tree位置

--- src/views/departments/departments.vue
+++ src/views/departments/departments.vue
@@ -25,10 +25,32 @@
               </el-row>
             </el-col>
           </el-row>
-      </el-card>  
-      <!-- 放置一个el-tree组件 -->
+          <!-- 放置一个el-tree组件 -->
         <el-tree :data="list" default-expand-all/>
-
+        <el-row type="flex" justify="space-between" align="middle" style="height: 40px width: 100%;">
+            <el-col :span="20">
+              <svg-icon icon-class="home" /><span>江苏传智播客教育科技股份有限公司</span>
+            </el-col>
+            <el-col :span="4">
+              <el-row type="flex" justify="end">
+                <!-- 两个内容 -->
+                <el-col>负责人</el-col>
+                <!-- 下拉菜单 element -->
+                <el-col>
+                  <el-dropdown>
+                    <span>
+                      操作<i class="el-icon-arrow-down" />
+                    </span>
+                    <el-dropdown-menu slot="dropdown">
+                      <el-dropdown-item>添加子部门</el-dropdown-item>
+                    </el-dropdown-menu>
+                  </el-dropdown>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+      </el-card>  
+      
     </div>
   </div>
 </template>
