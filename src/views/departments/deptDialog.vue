48e310443a6ca8beb6a2a4470b2a3a2367292aaf
lh-fish <1660031035@qq.com>
2022-06-10 20:54:19 +0800

    组织架构-表单的基本校验+兜底校验

--- src/views/departments/deptDialog.vue
+++ src/views/departments/deptDialog.vue
@@ -1,17 +1,17 @@
 <template>
-  <el-form ref="deptForm" label-width="120px">
-    <el-form-item label="部门名称">
+  <el-form ref="deptForm" :rules="rules" :model="form" label-width="120px">
+    <el-form-item label="部门名称" prop="name">
       <el-input v-model="form.name" style="width:80%" placeholder="1-50个字符" />
     </el-form-item>
-    <el-form-item label="部门编码">
+    <el-form-item label="部门编码" prop="code">
       <el-input v-model="form.code" style="width:80%" placeholder="1-50个字符" />
     </el-form-item>
-    <el-form-item label="部门负责人">
+    <el-form-item label="部门负责人" prop="manager">
       <el-select v-model="form.manager" style="width:80%" placeholder="请选择">
         <el-option v-for="item in list" :key="item.id" :label="item.username" :value="item.username" />
       </el-select>
     </el-form-item>
-    <el-form-item label="部门介绍">
+    <el-form-item label="部门介绍" prop="introduce">
       <el-input v-model="form.introduce" style="width:80%" placeholder="1-300个字符" type="textarea" :rows="3" />
     </el-form-item>
     <el-form-item>
@@ -38,6 +38,24 @@ export default {
   },
   data() {
     return {
+      // 表单校验
+      rules: {
+        name: [
+          { required: true, message: '部门名称不能为空', trigger: 'blur' },
+          { min: 1, max: 50, message: '部门名称要求1-50个字符', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '部门编码不能为空', trigger: 'blur' },
+          { min: 1, max: 50, message: '部门编码要求1-50个字符', trigger: 'blur' }
+        ],
+        manager: [
+          { required: true, message: '部门负责人不能为空', trigger: 'blur' }
+        ],
+        introduce: [
+          { required: true, message: '部门介绍不能为空', trigger: 'blur' },
+          { min: 1, max: 300, message: '部门介绍要求1-300个字符', trigger: 'blur' }
+        ]
+      },
       list: [],
       form: {
         name: '', // 部门名称
@@ -78,8 +96,13 @@ export default {
     },
     hSubmit() {
       // 表单校验
+      this.$refs.deptForm.validate(valid => {
+        if (valid) {
+          // 通过校验
+          this.isEdit ? this.doEdit() : this.doAdd()
+        }
+      })
       // 调用函数
-      this.isEdit ? this.doEdit() : this.doAdd()
     }
   }
 }
