5223defa18bf5c6c37e43b0ee5d76206d753e5b3
lh-fish <1660031035@qq.com>
2022-06-14 22:12:21 +0800

    员工管理-个人详情(上传头像)

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,14 @@ import PageTools from '@/components/PageTools/index.vue'
 
 // excel导入组件
 import UploadExcel from '@/components/UploadExcel'
+// 图片上传组件
+import UploadImg from '@/components/UploadImg'
 export default {
   install(Vue) {
     Vue.component('PageTools', PageTools)
-    // 注册UploadExcel组件
+    // 注册导入Excel组件
     Vue.component('UploadExcel', UploadExcel)
+    // 注册图片上传组件
+    Vue.component('UploadImg', UploadImg)
   }
 }
